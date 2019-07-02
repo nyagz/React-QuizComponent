f1504fa209a6ef3e8408aaf2adc323a6fa1e900e
Nyagz <44738554+nyagz@users.noreply.github.com>
2019-07-02 16:37:46 +0100

    Quiz mess around

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component{
     this.state = {quiz_position: 1}
   }
   render(
-    <div><div className = "QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+    <div><div className = "QuizQuestion">{quizData.quiz_questions[1].instruction_text}</div></div>
   )
 }
 
