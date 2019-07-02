3e2aa8b889963b5489e62a6884825b8e46912af8
Nyagz <44738554+nyagz@users.noreply.github.com>
2019-07-02 17:53:29 +0100

    Correction

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -16,9 +16,8 @@ class QuizQuestion extends Component{
         <section className = "buttons">
           <ul>
             {this.props.quiz_question.answer_options.map((answer_option, index) => {
-              return <QuizQuestionButton key = {index} button_text = {answer_option} />
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandle = {this.handleClick.bind(this)}/>
             })}
-            <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]} clickHandle = {this.handleClick.bind(this)} />
           </ul>
         </section>
       </main>
