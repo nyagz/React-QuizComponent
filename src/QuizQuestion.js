f1f2051a24aae3e692d6df1f8ffc714f753fe9f5
Nyagz <44738554+nyagz@users.noreply.github.com>
2019-07-03 11:05:00 +0100

    incorrectAnswer not inCorrectAnswer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -29,7 +29,7 @@ class QuizQuestion extends Component{
             })}
           </ul>
         </section>
-        {this.state.inCorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
